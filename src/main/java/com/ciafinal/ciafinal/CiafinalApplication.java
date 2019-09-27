65eb198a2f2953bf8c3d506d1abe58108a4c0d8a
Kannan Somaiah <kannansomaiah@hotmail.com>
2019-09-27 21:43:57 +0400

    first commit

---
+++ src/main/java/com/ciafinal/ciafinal/CiafinalApplication.java
@@ -0,0 +1,13 @@
+package com.ciafinal.ciafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiafinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiafinalApplication.class, args);
+	}
+
+}
